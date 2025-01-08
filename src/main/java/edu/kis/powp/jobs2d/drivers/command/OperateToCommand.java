302258bc6657e378a2d2a8400b30fd90f57560a5
abienias97 <abienias97@gmail.com>
2025-01-08 20:13:43 +0100

    add OperateTo and SetPosition commands

--- src/main/java/edu/kis/powp/jobs2d/drivers/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/OperateToCommand.java
@@ -1,2 +1,29 @@
-package edu.kis.powp.jobs2d.drivers.command;public class OperateToCommand {
+package edu.kis.powp.jobs2d.drivers.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class OperateToCommand implements DriverCommand {
+    private int x;
+    private int y;
+    private Job2dDriver job2ddriver;
+
+    public OperateToCommand(int x, int y, Job2dDriver driver) {
+        this.x = x;
+        this.y = y;
+        this.job2ddriver = driver;
+    }
+
+    public void setDriver(Job2dDriver driver) {
+        this.job2ddriver = driver;
+    }
+
+    public void operateTo(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void execute() {
+        job2ddriver.operateTo(x, y);
+    }
 }
